b757421c34dd2b009c36147704460a4710d6cafd
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-02-17 13:44:21 +0100

    Convert Navbar from class to function component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,17 @@
-import { Component } from "react"
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
-class Navbar extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			links: props.links,
-			current: "",
-		};
-	}
-
-	render() {
-		return (
-			<div className={styles.navbar + " grid-row space-between"}>
-				<span className={styles.logo}>mPlayer</span>
-				<div className={styles.navLinks + " grid-row grid-center gap-lg"}>
-					{this.state.links.map((link, i) => (
-						<NavLink exact={true} to={link.href} key={i}>{link.name}</NavLink>
-					))}
-				</div>
+function Navbar(props) {
+	return (
+		<div className={styles.navbar + " grid-row space-between"}>
+			<span className={styles.logo}>mPlayer</span>
+			<div className={styles.navLinks + " grid-row grid-center gap-lg"}>
+				{props.links.map((link, i) => (
+					<NavLink exact={true} to={link.href} key={i}>{link.name}</NavLink>
+				))}
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default Navbar;
